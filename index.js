3f14abce8a75ece9fcbb2936afe769a0fd080c94
xuanson <dohongson08052003@gmail.com>
2024-02-28 21:08:32 +0700

    express js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    let a = 1
+    let b = 2
+    let c = a + b
+
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}/`)
+})
